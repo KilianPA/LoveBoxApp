f2bc15fea7c4514f72dfa4f4b63cbfa4c1cc855b
Kilian <kilianpasini@hotmail.fr>
2021-11-06 16:04:38 +0100

    update server.js

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 var WebSocket = require('ws');
 var servo = require('./servo')
-const ws = new WebSocket('wss://websocket-love.herokuapp.com/');
+const address = 'wss://websocket-love.herokuapp.com/';
+const client = 'k'
+var timeout = 10; // seconds
 
-ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    servo.runAnimation()
-});
+connect(address);
+
+function connect(address) {
+    let ws = new WebSocket(address);
+    let timerTimeout = setTimeout(() => ws.terminate(), timeout * 1000); // force close unless cleared on 'open'
+    ws.on('open', () => {
+        console.log('Opened. Clearing timeout ...');
+        clearTimeout(timerTimeout);
+        // do your thing here, like ws.send(...);
+    });
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        let sender = (message.toString().split(';')[1])
+        if (sender !== client) {
+            servo.runAnimation()
+        }
+    });
+    ws.on('close', () => {
+        clearTimeout(timerTimeout);
+        console.error('Websocket connection closed. Reconnecting in %f seconds ...', timeout);
+        setTimeout(() => connect(address), timeout * 1000);
+    });
+    ws.on('error', reason => console.error('Websocket error: ' + reason.toString()));
+    return ws;
+}
+
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
